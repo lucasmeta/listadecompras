ec3eb953737694fb55405bae7643ba09a3b1bd33
unknown <jovem.tecnologia@GMEPD002140.metainf.com.br>
2018-07-21 11:10:14 -0300

    lista de compras atualizada

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+  <ul>
+    <li v-for="item in Compras">
+    {{ item }}
+    </li>
+    </ul>
+  <input type="text" v-model="Message"/>
+  <button @click="nomeDaMinhaFuncao()">Botao Teste</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app',
+  data: function() {
+    return{
+      message: 'Lista de Compras',
+      Compras: [
+        'Pão',
+        'Queijo',
+        'Mortadela',
+        'Maionese',
+      ]
+    }
+  },
+  methods: {
+    nomeDaMinhaFuncao(){
+      alert('kkkk');
+    },  
+  }
+}
+</script>
+<style>
+</style>
